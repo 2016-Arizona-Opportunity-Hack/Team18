535493bcadbefb6cd3fc00ab23553e5f12dbf7ab
Curtis Spence <ch.spence@yahoo.com>
2016-10-02 05:43:00 -0700

    Modal work continued

--- public/javascripts/shared.js
+++ public/javascripts/shared.js
@@ -12,7 +12,7 @@ function logout()
     url: logout_url,
     success: function(response_data)
     {
-      if(response_data.status == 200 && response_data.message == "Session Deleted")
+      if(response_data.status === 200 && response_data.message === "Session Deleted")
       {
         window.location.replace(getServer() + '/');
       }
@@ -34,7 +34,7 @@ function ShowModal(member_info)
   $('#member-modal-table-company').html(member_info.company);
   $('#member-modal-table-last-contacted').html(member_info.last_contacted);
   $('#member-modal-save-button').removeAttribute("onclick");
-  $('#member-modal-save-button').setAttribute( "onClick", "SaveMemberModalInfo(" + member_info.id + ");" );
+  $('#member-modal-save-button').setAttribute( "onclick", "SaveMemberModalInfo(" + member_info.id + ");" );
   $('#member-modal').modal({
     backdrop: 'static',
     keyboard: false
@@ -46,7 +46,7 @@ function SaveMemberModalInfo(member_id)
   if(member_id != null)
   {
     var info = {
-      'type': 2,
+      'type': ,
       'id': member_id,
       'first_name': $('#member-modal-table-first-name').html,
       'last_name': $('#member-modal-table-last-name').html,
@@ -59,22 +59,108 @@ function SaveMemberModalInfo(member_id)
       'preference': $('#member-modal-table-preference').html
     }
 
+    $.ajax({
+      type: "PUT",
+      url:  getServer() "/member/",
+      data: info,
+      success: function(response_data)
+      {
+        if(response_data.status === 200)
+        {
+          //Repopulate table
+          RepopulateCurrentMemberTable();
+        }
+        else if(response_data.status === 400)
+        {
+          console.log("Incorrect input");
+        }
+        else if(response_data.status === 500)
+        {
+          console.log("Server Error");
+        }
+        else
+        {
+          console.log("Error: " + response_data.status);
+        }
 
+      }
+    });
   }
   else
   {
     var info = {
-      'id': member_id,
+      'type': 2,
       'first_name': $('#member-modal-table-first-name').html,
       'last_name': $('#member-modal-table-last-name').html,
-      'phone': $('#member-modal-table-first-name').html,
-      'email': $('#member-modal-table-first-name').html,
-      'address': $('#member-modal-table-first-name').html,
-      'company': $('#member-modal-table-first-name').html,
-      'type': $('#member-modal-table-first-name').html,
-      'last_contacted': $('#member-modal-table-first-name').html,
-      'interest': $('#member-modal-table-first-name').html,
-      'preference': $('#member-modal-table-first-name').html
+      'phone': $('#member-modal-table-phone').html,
+      'email': $('#member-modal-table-email').html,
+      'address': $('#member-modal-table-address').html,
+      'company': $('#member-modal-table-company').html,
+      'last_contacted': $('#member-modal-table-last-contacted').html,
+      'interest': $('#member-modal-table-interest').html,
+      'preference': $('#member-modal-table-preference').html
+    }
+
+    $.ajax({
+      type: "POST",
+      url: getServer() "/member/",
+      data: info,
+      success: function(response_data)
+      {
+        if(response_data.status === 200 && response_data.message === 'Successfully accessed participant data' && response_data.member.id === member_id)
+        {
+          //Repopulate table
+          RepopulateCurrentMemberTable();
+        }
+        else {
+          //log error
+          console.log("Error: " + response_data.message);
+        }
+      }
+    });
+  }
+
+  function RepopulateCurrentMemberTable()
+  {
+    var curTable = $(#current-member-table-body);
+    if(curTable.name === 'donors-table-body')
+    {
+      //Remove all table rows and repopulate, no donor adding
+      $(#current-member-table-body).remove();
+      setup();
+    }
+    else if(curTable.name === 'participants-table-body')
+    {
+      //Remove 2nd row and onward rows and repopulate, keep add row
+      var i;
+      for(i = $(#current-member-table-body).rows.length - 1; i > 0; i--)
+      {
+        $(#current-member-table-body).removeRow(i);
+      }
+      setup();
+    }
+    else if(curTable.name === 'admins-table-body')
+    {
+      //if superadmin -> add row exists so remove 2nd index onward ... else remove all rows
+      if(session.type === 'regular')
+      {
+        $(#current-member-table-body).remove();
+        setup();
+      }
+      else
+      {
+        var i;
+        for(i = $(#current-member-table-body).rows.length - 1; i > 0; i--)
+        {
+          $(#current-member-table-body).removeRow(i);
+        }
+        setup();
+      }
+
+    }
+    else
+    {
+        console.log("Error: tableName = " + tableName);
     }
   }
 }
