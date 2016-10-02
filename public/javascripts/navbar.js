6d0ab72863ed836ddc6568be1f00b77c483db735
Curtis Spence <ch.spence@yahoo.com>
2016-10-01 19:03:54 -0700

    Home page navbar modeling

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,26 +1,22 @@
-'use strict';
-
-function getServer() {
-  return location.protocol + '//' + location.hostname + ':' + location.port;
-}
+'use strict'
 
 function logout()
 {
-  console.log("here9");
+  console.log("here");
   $.ajax({
     type: "DELETE",
     url: login_url,
     data: login_info,
     success: function(response_data)
     {
-      console.log("here82");
+      console.log("here2");
       if(response_data.status == 200 && response_data.message == "Session Deleted")
       {
-        console.log("here73");
+        console.log("here3");
         window.location.replace(getServer() + '/');
       }
       else {
-        console.log("here64 " + response_data.status + ", " + response_data.message);
+        console.log("here4 " + response_data.status + ", " + response_data.message);
       }
     }
   });
